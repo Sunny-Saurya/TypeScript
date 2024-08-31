dce632f6af8a45b60323ab45df1af6d287195601
Sunny Kumar <sunnysaurya9955@gmail.com>
2024-08-31 18:27:20 +0530

    intro file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("shfsdkfskfj")
+console.log("sunny kumar")
+
+function greet(firstName:string, lastName: string, age:number){
+    console.log("Hello " + firstName, lastName, age);
+}
+greet("Sunny ", "Kumar ", 19); 
